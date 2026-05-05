8daeda31b3c9155fa26a3b4f1dcf1c2be2e6be46
ambokileB <wholemike2703email.com>
2026-05-05 10:00:32 +0300

    feat: initial changes made

--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,6 +1,5 @@
 
-// import { ForgotPassword } from "./forgot_Password/ForgotPassword";
-// import { ResetPassword } from "./resetPassword/ResetPassword";
-import { Auth } from "./Auth";
-// export { ForgotPassword, ResetPassword,Auth };
+import { ForgotPassword } from "./forgot_Password/ForgotPassword";
+import { ResetPassword } from "./resetPassword/ResetPassword";
+export { ForgotPassword, ResetPassword,Auth };
 export { Auth };
